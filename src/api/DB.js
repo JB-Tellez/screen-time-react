ecf12fb937d28e172e66a5e345f845506f4656b2
Jonathyn Tellez <jbtellez@Jonathyns-MacBook-Pro.local>
2017-09-20 08:50:06 -0700

    add viewings data

--- src/api/DB.js
+++ src/api/DB.js
@@ -15,13 +15,51 @@ export default class DB {
                     "id": "1",
                     "name": "Millie",
                     "gender": "female",
-                    "viewings": []
+                    "viewings": [
+                        {
+                          "_id": "598ced985fab070011800d70",
+                          "title": "Wonder Woman",
+                          "movieId": "297762",
+                          "kid": "598ced7d5fab070011800d6f",
+                          "__v": 0,
+                          "endTime": "2017-09-19T23:34:48.440Z",
+                          "startTime": "2017-09-19T22:34:48.440Z"
+                        },
+                        {
+                          "_id": "598cef1a5fab070011800d72",
+                          "title": "Fantastic 4: Rise of the Silver Surfer",
+                          "movieId": "1979",
+                          "kid": "598ced7d5fab070011800d6f",
+                          "__v": 0,
+                          "endTime": "2017-09-18T23:41:14.286Z",
+                          "startTime": "2017-09-18T21:41:14.286Z"
+                        }
+                      ]
                 },
                 {
                     "id": "2",
                     "name": "Michael",
                     "gender": "male",
-                    "viewings":[]
+                    "viewings":[
+                        {
+                            "_id": "598ced985fab070011800d70",
+                            "title": "Wonder Woman",
+                            "movieId": "297762",
+                            "kid": "598ced7d5fab070011800d6f",
+                            "__v": 0,
+                            "endTime": "2017-09-16T23:34:48.440Z",
+                            "startTime": "2017-09-16T22:34:48.440Z"
+                          },
+                          {
+                            "_id": "598cef1a5fab070011800d72",
+                            "title": "Fantastic 4: Rise of the Silver Surfer",
+                            "movieId": "1979",
+                            "kid": "598ced7d5fab070011800d6f",
+                            "__v": 0,
+                            "endTime": "2017-09-17T23:41:14.286Z",
+                            "startTime": "2017-09-17T21:41:14.286Z"
+                          }
+                    ]
                 }]  
       }
 
